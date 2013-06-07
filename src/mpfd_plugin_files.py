0e3673fb2826c838a6260c51feb855124f7d40bd
Jack Haughton <j.s.haughton.03@cantab.net>
2013-06-07 17:01:28 +0100

    A little more work on the file plugin

--- src/mpfd_plugin_files.py
+++ src/mpfd_plugin_files.py
@@ -9,16 +9,20 @@ import mpfd
 class MPFDFilesPlugin:
     def __init__(self, root, mountpoint):
         self.root=root
-		self.mountpoint=mountpoint
-		
-	def listDir(dir):
-		if dir.startswith(mountpoint):
-			subdir=dir[len(mountpoint):].strip(mpfd.path_separator)
-			
-		else:
-			mpPartitioned=mountpoint.rpartition(mpfd.path_separator)
-			if dir.startswith(mpPartitioned[0]):
-				return [{'name':mpPartitioned[2], 'type': 'dir'}]
+        self.mountpoint=mountpoint
+        self.filePlayerPlugins=None
+    
+    def getFilePlayers(self):
+        self.filePlayerPlugins=[x for x in mpfd.plugins if hasattr(x, "fileFilter")]
+                
+    def listDir(self, dir):
+        if dir.startswith(mountpoint):
+            subdir=dir[len(mountpoint):].strip(mpfd.path_separator)
+          
+        else:
+            mpPartitioned=mountpoint.rpartition(mpfd.path_separator)
+            if dir.startswith(mpPartitioned[0]):
+                return [{'name':mpPartitioned[2], 'type': 'dir'}]
 
 def createInstance(config):
     return MPFDFilesPlugin(config['root'], config['mountpoint'])
